994c8c4aaaf5c5014b178511d5668910b3866e90
josdic1 <joshuadicker@gmail.com>
2025-01-08 13:06:52 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// code your solution here
+let sundayIntro = "This Saturday, I"
+let mondayIntro = "This Monday, I"
+
+function saturdayFun(activity) {
+   switch(activity) {
+      case 'bathe my dog':
+             return `${sundayIntro} want to bathe my dog!`
+      default:
+         return `${sundayIntro} want to roller-skate!`
+   }
+}
+
+function mondayWork(activity) {
+   switch(activity) {
+      case 'work from home':
+         return `${mondayIntro} will work from home.`
+      default:
+         return `${mondayIntro} will go to the office.`
+   }
+}
+
+
+
+function wrapAdjective(symbol) {
+   const prefix = "You are"
+   return function (emphatic) {
+      return (`${prefix} ${symbol}${emphatic}${symbol}!`)
+   }
+}
+
+wrapAdjective("*")("a hard worker")
