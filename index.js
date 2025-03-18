e05b5ba0d94238e347e968a5d9d7ebbb7fbe5dad
Josh Dicker <joshdicker@joshs-mac-mini.mynetworksettings.com>
2025-03-18 18:21:38 -0400

    functions continued done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(arg) {
+    const phrase = 'This Saturday, I want to '
+    return !arg ? phrase + 'roller-skate!' :
+        phrase + arg + "!"
+}
+
+function mondayWork(arg) {
+    const phrase =  "This Monday, I will "
+    return !arg ? phrase + 'go to the office.' :
+        phrase + arg + "."
+}
+
+function wrapAdjective(wrapper) {
+    return function(adjective) {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}
